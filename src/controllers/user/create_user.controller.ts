e03aab0c58a3b832ff6828225e8f00a75cc15b61
Mapinko <mapincosf@gmail.com>
2022-09-06 12:56:12 -0300

    fix (await): create user controller

--- src/controllers/user/create_user.controller.ts
+++ src/controllers/user/create_user.controller.ts
@@ -3,7 +3,12 @@ import createUserService from '../../services/user/create_user.service'
 
 const createUserController = async (req: Request, res: Response) => {
 	const { age, email, name, password } = req.body
-	const createNewUser = createUserService({ age, email, name, password })
+	const createNewUser = await createUserService({
+		age,
+		email,
+		name,
+		password,
+	})
 	return response.status(201).json({
 		message: 'User Created',
 		user: createNewUser,

1a0d7e9f5b7b793d7da4e826d0846f96bff40ad9
Mapinko <mapincosf@gmail.com>
2022-09-06 12:55:36 -0300

    feat: create user controller

--- src/controllers/user/create_user.controller.ts
+++ src/controllers/user/create_user.controller.ts
@@ -1,9 +1,9 @@
-import { response } from 'express'
+import { Request, response } from 'express'
 import createUserService from '../../services/user/create_user.service'
 
 const createUserController = async (req: Request, res: Response) => {
-	const newUser = req.body
-	const createNewUser = createUserService(newUser)
+	const { age, email, name, password } = req.body
+	const createNewUser = createUserService({ age, email, name, password })
 	return response.status(201).json({
 		message: 'User Created',
 		user: createNewUser,

9ad3d3d803334ba3ae98c8fa066a4a1751cdeb5e
Mapinko <mapincosf@gmail.com>
2022-09-06 16:15:26 -0300

    fix: parameters

--- src/controllers/user/active_user.controller.ts
+++ src/controllers/user/active_user.controller.ts
@@ -3,8 +3,8 @@ import { Request } from 'express-serve-static-core'
 import activateUserService from '../../services/email/activate_user_service'
 
 const activateUserController = async (req: Request, res: Response) => {
-	const { isActive, id } = req.user
-	await activateUserService(isActive, id)
+	const { id } = req.user
+	await activateUserService(id)
 	return res.json({ message: 'Your email has been successfully confirmed' })
 }
 export default activateUserController

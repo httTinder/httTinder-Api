31ade3a5673bb8cdc635e539f3184ee68f68e60d
Mapinko <mapincosf@gmail.com>
2022-09-06 16:14:03 -0300

    feat: email auth route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express'
 import activateUserController from '../controllers/user/active_user.controller'
 import createUserController from '../controllers/user/create_user.controller'
+import authEmailMiddleware from '../middlewares/authEmail.middleware'
+import { verifyActiveMiddleware } from '../middlewares/verifyActive.middleware'
 
 const usersRoutes = Router()
 
 usersRoutes.post('', createUserController)
-usersRoutes.patch('/email', activateUserController)
+
+usersRoutes.patch(
+	'/email/:id',
+	authEmailMiddleware,
+	verifyActiveMiddleware,
+	activateUserController
+)
 
 usersRoutes.get('')
 

8c002034468d59572a524174d040f5caa8d5f7a9
lmfreire <l.matheus.freire10@gmil.com>
2022-09-06 10:51:51 -0300

    feat-config-testes-initial

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,25 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB,
-  logging: true,
-  synchronize: true,
-  entities: ["src/entities/**/*.ts"],
-  migrations: ["src/migrations/*.ts"],
-});
+const AppDataSource =
+  process.env.NODE_ENV === "test"
+    ? new DataSource({
+        type: "sqlite",
+        database: ":memory:",
+        entities: ["src/entities/**/*.ts"],
+        synchronize: true,
+      })
+    : new DataSource({
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
+        logging: true,
+        synchronize: true,
+        entities: ["src/entities/**/*.ts"],
+        migrations: ["src/migrations/*.ts"],
+      });
 
 export default AppDataSource;

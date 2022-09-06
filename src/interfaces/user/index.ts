facbe174ac7768cf29e06c907383c2637c25d7af
fjrjdev <thedromemafia@gmail.com>
2022-09-06 15:53:46 -0300

    feat: user edit services, routes and controllers

---
+++ src/interfaces/user/index.ts
@@ -0,0 +1,6 @@
+export interface IUserEditRequest {
+  name?: string;
+  email?: string;
+  password?: string;
+  age?: number;
+}
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { userListController } from "../controllers/user/userList.controller";
 import { verifyAuthMiddleware } from "../middlewares/auth.middleware";
 import { verifyAdminMiddleware } from "../middlewares/verifyAdmin.middleware";
+import { userEditController } from "../controllers/user/userEdit.controller";
 
 const usersRoutes = Router();
 
@@ -10,6 +11,13 @@ usersRoutes.get(
   verifyAuthMiddleware,
   verifyAdminMiddleware,
   userListController
+);
+
+usersRoutes.patch(
+  "/:id",
+  verifyAuthMiddleware,
+  verifyAdminMiddleware,
+  userEditController
 );
 
 export default usersRoutes;

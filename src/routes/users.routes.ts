9907f07fc0bbe140b05d721be763d97eeb2d5ea2
lmfreire <l.matheus.freire10@gmil.com>
2022-09-07 13:06:32 -0300

    feat-Update-user-address-doone

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,20 +3,26 @@ import { userListController } from "../controllers/user/userList.controller";
 import { verifyAuthMiddleware } from "../middlewares/auth.middleware";
 import { verifyAdminMiddleware } from "../middlewares/verifyAdmin.middleware";
 import { userEditController } from "../controllers/user/userEdit.controller";
-import activateUserController from '../controllers/user/active_user.controller'
-import createUserController from '../controllers/user/create_user.controller'
-import authEmailMiddleware from '../middlewares/authEmail.middleware'
-import { verifyActiveMiddleware } from '../middlewares/verifyActive.middleware'
+import activateUserController from "../controllers/user/active_user.controller";
+import createUserController from "../controllers/user/create_user.controller";
+import authEmailMiddleware from "../middlewares/authEmail.middleware";
+import { verifyActiveMiddleware } from "../middlewares/verifyActive.middleware";
 import { adminPermission } from "../middlewares/adminPermission.middleware";
 import userDeleteController from "../controllers/user/userDelete.controller";
 import listUsersController from "../controllers/user/list_user.controller";
 import { editUserMiddleWare } from "../middlewares/editUser.middleware";
 import { upload } from "../utils/cloudinary.utils";
 import { imageEditController } from "../controllers/user/user_profile/user_images/imageEdit.controller";
+import updateUserAddressController from "../controllers/user/user_address/update_user_address.controller";
 
-const usersRoutes = Router()
+const usersRoutes = Router();
 
-usersRoutes.get("/all", verifyAuthMiddleware, verifyAdminMiddleware, listUsersController)
+usersRoutes.get(
+  "/all",
+  verifyAuthMiddleware,
+  verifyAdminMiddleware,
+  listUsersController
+);
 
 usersRoutes.get(
   "/data/:id?",
@@ -41,35 +47,42 @@ usersRoutes.patch(
   userEditController
 );
 
-usersRoutes.post('', createUserController)
+usersRoutes.post("", createUserController);
+
+usersRoutes.patch("/email/:tokenEmail", activateUserController);
 
 usersRoutes.patch(
-	'/email/:tokenEmail',
-	activateUserController
-)
+  "/address/:id",
+  verifyAuthMiddleware,
+  adminPermission,
+  updateUserAddressController
+);
 
-usersRoutes.patch("/address/:id")
+usersRoutes.patch("/profile/:id");
 
-usersRoutes.patch("/profile/:id")
+usersRoutes.patch("/lookingfor/:id");
 
-usersRoutes.patch("/lookingfor/:id")
+usersRoutes.patch("/relationship/:id");
 
-usersRoutes.patch("/relationship/:id")
+usersRoutes.patch(
+  "/images",
+  verifyAuthMiddleware,
+  upload.array("image", Infinity),
+  imageEditController
+); // falta o delete
 
-usersRoutes.patch("/images", verifyAuthMiddleware, upload.array("image", Infinity), imageEditController) // falta o delete
+usersRoutes.patch("/additional/:id");
 
-usersRoutes.patch("/additional/:id")
+usersRoutes.delete("/address/:id");
 
-usersRoutes.delete("/address/:id")
+usersRoutes.delete("/profile/:id");
 
-usersRoutes.delete("/profile/:id")
+usersRoutes.delete("/lookingfor/:id");
 
-usersRoutes.delete("/lookingfor/:id")
+usersRoutes.delete("/relationship/:id");
 
-usersRoutes.delete("/relationship/:id")
+usersRoutes.delete("/images"); // falta o delete
 
-usersRoutes.delete("/images") // falta o delete
+usersRoutes.delete("/additional/:id");
 
-usersRoutes.delete("/additional/:id")
-
-export default usersRoutes
+export default usersRoutes;

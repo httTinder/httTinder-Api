162ca680bc98d5575ad38b1d26ce8c4cc25726de
lmfreire <l.matheus.freire10@gmil.com>
2022-09-07 14:50:24 -0300

    fix-remove-try-catch

--- src/controllers/user/user_address/delete_user_address.controller.ts
+++ src/controllers/user/user_address/delete_user_address.controller.ts
@@ -1,23 +1,16 @@
 import { Request, Response } from "express";
-import { AppError } from "../../../errors/AppError";
 import userDeleteAddressService from "../../../services/user/user_address/delete_user_address.service";
 
 const userDeleteAddressController = async (req: Request, res: Response) => {
-  try {
-    let id = req.user.id;
+  let id = req.user.id;
 
-    if (req.params.id !== undefined) {
-      id = req.params.id;
-    }
-
-    await userDeleteAddressService(id);
-
-    return res.status(204).json({ message: "Address deleted with sucess!" });
-  } catch (error) {
-    if (error instanceof AppError) {
-      res.status(error.statusCode).json({ message: error.message });
-    }
+  if (req.params.id !== undefined) {
+    id = req.params.id;
   }
+
+  await userDeleteAddressService(id);
+
+  return res.status(204).json({ message: "Address deleted with sucess!" });
 };
 
 export default userDeleteAddressController;
--- src/controllers/user/user_address/update_user_address.controller.ts
+++ src/controllers/user/user_address/update_user_address.controller.ts
@@ -1,27 +1,20 @@
 import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
-import { AppError } from "../../../errors/AppError";
 import { IUserAdressRequest } from "../../../interfaces/user/user_address";
 import updateUserAddressService from "../../../services/user/user_address/update_user_address.service";
 
 const updateUserAddressController = async (req: Request, res: Response) => {
-  try {
-    const userData: IUserAdressRequest = req.body;
-    let { id } = req.user;
+  const userData: IUserAdressRequest = req.body;
+  let { id } = req.user;
 
-    if (req.params.id !== undefined) {
-      id = req.params.id;
-    }
-
-    await updateUserAddressService(userData, id);
-    return res
-      .status(201)
-      .json(instanceToPlain({ message: "Address changed successfully" }));
-  } catch (error) {
-    if (error instanceof AppError) {
-      res.status(error.statusCode).json({ message: error.message });
-    }
+  if (req.params.id !== undefined) {
+    id = req.params.id;
   }
+
+  await updateUserAddressService(userData, id);
+  return res
+    .status(201)
+    .json(instanceToPlain({ message: "Address changed successfully" }));
 };
 
 export default updateUserAddressController;

d67c8634a8aa942b5507779bedeb58c27345de53
Igor <igorgg@gmail.com>
2022-09-07 17:17:33 -0300

    feat: patch and delete done

---
+++ src/services/user/user_profile/looking_for/deleteLookingFor.service.ts
@@ -0,0 +1,23 @@
+import { lookingFor } from "./../../../../entities/user_profile/looking_for/index";
+import { AppError } from "./../../../../errors/AppError";
+import AppDataSource from "../../../../data-source";
+import { user } from "../../../../entities";
+
+export const deleteLookingForService = async (userId: string) => {
+  const userRepository = AppDataSource.getRepository(user);
+  const lookingForRepository = AppDataSource.getRepository(lookingFor);
+
+  const userFind = await userRepository.findOne({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!userFind) {
+    throw new AppError(404, "User not found");
+  }
+
+  await lookingForRepository.delete({ id: userFind.profile.lookingFor.id });
+
+  return;
+};

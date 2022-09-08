8dfb635dbf00be5be04b7447fc603ae5510ae30a
fjrjdev <thedromemafia@gmail.com>
2022-09-08 10:53:09 -0300

    FIX: server port

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import AppDataSource from "./data-source";
   await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Sou rce Initialization", err);
   });
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor Executando");
   });
 })();

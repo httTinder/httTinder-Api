34acb03d08588250d0995cb3ee660ad543fedab6
Mapinko <mapincosf@gmail.com>
2022-09-08 13:23:54 -0300

    fix: indentation

--- src/services/userSession/userSession.service.ts
+++ src/services/userSession/userSession.service.ts
@@ -1,42 +1,42 @@
-import jwt from "jsonwebtoken";
-import { compare, compareSync } from "bcryptjs";
-import AppDataSource from "../../data-source";
-import { user } from "../../entities";
-import { AppError } from "../../errors/AppError";
-import { IUserSession } from "../../interfaces/user/userSession";
+import { compareSync } from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import AppDataSource from '../../data-source'
+import { user } from '../../entities'
+import { AppError } from '../../errors/AppError'
+import { IUserSession } from '../../interfaces/user/userSession'
 
-const userRepository = AppDataSource.getRepository(user);
+const userRepository = AppDataSource.getRepository(user)
 
 export const userSessionService = async ({ email, password }: IUserSession) => {
-  const findUser = await userRepository.findOne({
-    where: {
-      email,
-    },
-  });
+	const findUser = await userRepository.findOne({
+		where: {
+			email,
+		},
+	})
 
-  if (!findUser) {
-    throw new AppError(403, "Email or Password not match");
-  }
+	if (!findUser) {
+		throw new AppError(403, 'Email or Password not match')
+	}
 
-  const verifyPassword = compareSync(password, findUser.password);
+	const verifyPassword = compareSync(password, findUser.password)
 
-  if (!verifyPassword) {
-    throw new AppError(403, "Email or Password not match");
-  }
+	if (!verifyPassword) {
+		throw new AppError(403, 'Email or Password not match')
+	}
 
-  if (!findUser.isActive) {
-    throw new AppError(403, "User is not active");
-  }
+	if (!findUser.isActive) {
+		throw new AppError(403, 'User is not active')
+	}
 
-  const token = jwt.sign(
-    {
-      isAdm: findUser.isAdm,
-      isActive: findUser.isActive,
-      id: findUser.id,
-    },
-    process.env.SECRET_KEY as string,
-    { expiresIn: "24h", subject: findUser.id }
-  );
+	const token = jwt.sign(
+		{
+			isAdm: findUser.isAdm,
+			isActive: findUser.isActive,
+			id: findUser.id,
+		},
+		process.env.SECRET_KEY as string,
+		{ expiresIn: '24h', subject: findUser.id }
+	)
 
-  return token;
-};
+	return token
+}

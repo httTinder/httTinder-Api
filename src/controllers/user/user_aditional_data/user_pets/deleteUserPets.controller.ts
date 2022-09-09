9fadf23517961a99afbbed3b17b00718f873ebb4
Fábio Casanova <98760788+fabiojcp@users.noreply.github.com>
2022-09-09 17:21:27 -0300

    Update deleteUserPets.controller.ts

--- src/controllers/user/user_aditional_data/user_pets/deleteUserPets.controller.ts
+++ src/controllers/user/user_aditional_data/user_pets/deleteUserPets.controller.ts
@@ -2,9 +2,11 @@ import { Request, Response } from "express";
 import { deletePetsService } from "../../../../services/user/user_aditional_data/user_pets/deletePets.service";
 
 export const deleteUserPetsController = async (req: Request, res: Response) => {
+  const uuid = req.body.uuid
+  
   const id = req.idParams.id;
 
-  await deletePetsService(id);
+  await deletePetsService(id, uuid);
 
   return res.json({ message: "Pet Deleted" });
 };
